b6b6b2f93800657db8dde3f35bd842d1c944abf8
adam-bekila1 <adam.bekila@sky.uk>
2021-05-24 00:22:26 +0100

    fix(revert sa attachement): reverting back the service account + IAM tf resource attachement
    Signed-off-by: adam-bekila1 <adam.bekila@sky.uk>

--- examples/cron_jobs/main.tf
+++ examples/cron_jobs/main.tf
@@ -1,22 +1,20 @@
 module "cloud_scheduler" {
-  for_each = var.cloud_schduler_config
+  for_each = var.cloud_scheduler_config
 
-  source             = "../../modules/cloud_scheduler"
-  project_id         = var.project_id
-  region             = var.region
-  environment        = var.environment
-  scheduler_name     = each.key
-  description        = lookup(each.value, "description", null)
-  scheduler_sa_id    = lookup(each.value, "scheduler_sa_id")
-  sa_display_name    = lookup(each.value, "sa_display_name", null)
-  scheduler_sa_roles = lookup(each.value, "scheduler_sa_roles", [])
-  schedule           = lookup(each.value, "schedule")
-  time_zone          = lookup(each.value, "time_zone")
-  http_method        = lookup(each.value, "http_method")
-  uri                = lookup(each.value, "uri")
-  oidc_token_enable  = lookup(each.value, "oidc_token_enable", false)
-  audience           = lookup(each.value, "audience", null)
-  oauth_token_enable = lookup(each.value, "oauth_token_enable", false)
-  scope              = lookup(each.value, "scope", null)
-  body               = lookup(each.value, "body", "")
+  source                = "../../modules/cloud_scheduler"
+  project_id            = var.project_id
+  region                = var.region
+  environment           = var.environment
+  scheduler_name        = each.key
+  description           = lookup(each.value, "description", null)
+  service_account_email = lookup(each.value, "service_account_email")
+  schedule              = lookup(each.value, "schedule")
+  time_zone             = lookup(each.value, "time_zone")
+  http_method           = lookup(each.value, "http_method")
+  uri                   = lookup(each.value, "uri")
+  oidc_token_enable     = lookup(each.value, "oidc_token_enable", false)
+  audience              = lookup(each.value, "audience", null)
+  oauth_token_enable    = lookup(each.value, "oauth_token_enable", false)
+  scope                 = lookup(each.value, "scope", null)
+  body                  = lookup(each.value, "body", "")
 }
